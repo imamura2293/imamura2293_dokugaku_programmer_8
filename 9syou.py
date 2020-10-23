b45cbd5d5866f0fa6d05241a0859d4338a9237c7
imamura2293 <uu77ee22dd55>
2020-10-24 08:17:59 +0900

    9syou

--- 9syou.py
+++ 9syou.py
@@ -66,23 +66,26 @@ class Orange:
     self.color = c
     print("Created!")
 
+
 class Orange:
     def__init__(self, w, c):
     self.weight = w
     self.color = c
     print("Created!")
 
-or1 = Orange(10,"dark orange")
+
+or1 = Orange(10, "dark orange")
 print(or1)
 
+
 class Orange:
-    def__init__(self,w,c):
-    self.weight=w
-    self.color =c
+    def__init__(self, w, c):
+    self.weight = w
+    self.color = c
     print(Created!)
-or1 = Orange(10,"dark orange")
-print(or1.weight)
-print(or1.color)
+    or1 = Orange(10, "dark orange")
+    print(or1.weight)
+    print(or1.color)
 
     class Orange:
         def__init__(self, w, c):
@@ -90,5 +93,52 @@ print(or1.color)
         self.color = c
         print(Created!)
         or1 = Orange(10, "dark orange")
+        or1 = "light orange"
+
         print(or1.weight)
         print(or1.color)
+
+        class Orange:
+            def__init__(self, w, c):
+            self.weight = w
+            self.color = c
+            print(Created!)
+
+            or1 = Orange(4, "light orange")
+            or2 = Orange(8, "dark orange")
+            or3 = Orange(14, "yellow")
+
+    class Orange:
+        def__init__(self, w, c):
+        """weight(重さ)はグラム"""
+        self.weight = w
+        self.color = c
+        seif.mind = 0
+        print("Created!")
+
+        def rot(self, days, temp):
+            """temp（温度）は摂氏"""
+            self.mold = days * temp
+
+orange Orange(200, "orange")
+print(orange.mold)
+orange.rot(10,37)
+print(orange.mold)
+
+class Rectangle:
+    def__init__(self,w,l):
+        self.width = w
+        self.len = 1
+
+    def area(self):
+        return self.width * self.len
+
+    def change_size(self,w,1):
+        self.width = w
+        self.len = l
+
+rectangle = Rectangle(10,20)
+print(rectangle.area())
+rectangle.change_size(20,40)
+print(rectangle.area())
+
